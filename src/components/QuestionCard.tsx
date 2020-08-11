bd9684a2d072b1d4ee3fa9c1749026c3c2b04a4f
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-08-11 13:34:22 +0500

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,35 +1,38 @@
-import React from 'react'
+import React from 'react';
+// Types
 import { AnswerObject } from '../App';
 
-
 type Props = {
-    question: string;
-    answers: string[];
-    callback:  (e: React.MouseEvent<HTMLButtonElement>) => void;
-    userAnswer: AnswerObject | undefined;
-    questionNr: number;
-    totalQuestions: number;
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
 };
+
 const QuestionCard: React.FC<Props> = ({
-    question,
-    answers,
-    callback,
-    userAnswer,
-    questionNr,
-    totalQuestions,
-}) => {
-<div>
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
     <p className='number'>
       Question: {questionNr} / {totalQuestions}
     </p>
     <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
       {answers.map((answer) => (
-     <div key={answer}>
+        <div
+          key={answer}
+        >
           <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
-</div>
+        </div>
       ))}
     </div>
   </div>
