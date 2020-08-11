060dbc63f5cf1dd73306d93abffd935dd94e27d6
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-08-11 13:08:40 +0500

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -18,26 +18,19 @@ const QuestionCard: React.FC<Props> = ({
     questionNr,
     totalQuestions,
 }) => {
-    return (
-        <div>
-            <p className="number">
-                Questions: {questionNr} / {totalQuestions}
-            </p>
-            <p dangerouslySetInnerHTML={{__html: question}} />
-<div>
-    if (answers.map) {
-(answer => (
-<div key={answer}>
-    <button disabled={!!userAnswer} value={answer} onClick={callback}>
-        <span dangerouslySetInnerHTML={{__html: answer }} />
-    </button>
-</div>) } else {
-console.log('error') };
-    ))}
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+     <div key={answer}>
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
 </div>
-    
-        </div>
-    )
-}
+      ))}
+  </div>
+);
 
 export default QuestionCard;
