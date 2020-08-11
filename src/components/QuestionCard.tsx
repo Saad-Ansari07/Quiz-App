f81983df4e2db9d58f1d5fd810074583d510c654
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-08-11 16:48:23 +0500

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -11,7 +11,7 @@ type Props = {
   totalQuestions: number;
 };
 
-function AnswerList({ answers, fallback }) {
+function AnswerList({ answers: string[], fallback }) {
   if (!answers || answers.length === 0) {
     return fallback;
   } else {
