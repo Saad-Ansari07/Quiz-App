49effa0da7c2f33546a02f8767f525fdd3cdc3af
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-08-14 13:43:56 +0500

    Update QuestionCard.styles.ts

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  max-width: 600px;
+  max-width: 2000px;
   background: #ebfeff;
   border-radius: 10px;
   border: 2px solid #0085a3;
@@ -9,7 +9,6 @@ export const Wrapper = styled.div`
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
   text-align: center;
   justify-content: center;
-  margin-right: 50%;
   p {
     font-size: 1rem;
   }
