ef01b506bf9b50c6ae5379525a9149eac52faa22
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-08-14 13:39:10 +0500

    Update QuestionCard.styles.ts

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -9,6 +9,7 @@ export const Wrapper = styled.div`
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
   text-align: center;
   justify-content: center;
+  margin-right: 50%;
   p {
     font-size: 1rem;
   }
